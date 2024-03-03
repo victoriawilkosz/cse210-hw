0f8951f9edda02b0e80bacf2b26d9554f22b026d
victoriawilkosz <victoriawilkosz@gmail.com>
2024-03-03 19:37:22 +0000

    assignment prep 2 finished

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -3,7 +3,44 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
+       {
+        Console.Write("Enter your grade percentage: ");
+        int gradePercentage = int.Parse(Console.ReadLine());
+
+        string letter;
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Keep working hard for next time!");
+        }
+
+        Console.ReadLine();
     }
 }
