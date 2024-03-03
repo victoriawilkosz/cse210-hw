ccaabd04c04562d0bda26be883b6e36aae30baa8
victoriawilkosz <victoriawilkosz@gmail.com>
2024-03-03 19:51:15 +0000

    prep 3 finished assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,35 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+ {
+    //  Console.Write("What is the magic number? ");
+    //  int magicNumber = int.Parse(Console.ReadLine());
+    
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        } while (guess != magicNumber);
+
+        Console.ReadLine();
     }
 }
