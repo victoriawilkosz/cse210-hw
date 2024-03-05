74d311ea3e03a1cb040499c2e8a0d82be1e56b2a
victoriawilkosz <victoriawilkosz@gmail.com>
2024-03-05 17:28:37 +0000

    finished code

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "UX/UI Designer";
+        job1._company = "Apple";
+        job1._startYear = 2018;
+        job1._endYear = 2021;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Graphic Designer";
+        job2._company = "Google";
+        job2._startYear = 2021;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Emma Smith";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
